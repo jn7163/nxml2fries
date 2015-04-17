f1e982b8d6b7fa448b08c906729e6050edc09687
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-04-17 10:49:20 -0700

    added if __main__ and command line args

--- nxml2fries.py
+++ nxml2fries.py
@@ -2,6 +2,7 @@
 nxml2txt output'''
 
 import sys
+import argparse
 import itertools as it
 
 nil = 'N/A'
@@ -28,102 +29,111 @@ def get_text(txt, start, end, citations):
     # Return the filtered string
     return ''.join(txt[r[0]:r[1]] for r in ranges)
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='parse nxml file and dump sections')
+    parser.add_argument('--no-xref', action='store_true', help='replace citations with spaces')
+    parser.add_argument('textfile', help='text file')
+    parser.add_argument('standoff', help='standoff file')
+    return parser.parse_args()
 
-# Read the text file
-with open(sys.argv[1]) as f:
-    txt = f.read()
+if __name__ == '__main__':
+    args = parse_args()
 
-# Read the standoff file
-with open(sys.argv[2]) as f:
-    soff = f.readlines()
+    # Read the text file
+    with open(args.textfile) as f:
+        txt = f.read()
 
-keep = ('article-title', 'abstract', 'sec', 'title', 'fig', 'p', 'supplementary-material')
-remove = ('xref')
+    # Read the standoff file
+    with open(args.standoff) as f:
+        soff = f.readlines()
 
-# Compute the spans of the citations to remove them from the text
-citations = []
+    keep = ('article-title', 'abstract', 'sec', 'title', 'fig', 'p', 'supplementary-material')
+    remove = ('xref')
 
-for line in (s for s in soff if s.split('\t', 2)[1].split()[0] in remove):
-    # Parse the line
-    _, t_r, __ = line.split('\t', 2)
+    # Compute the spans of the citations to remove them from the text
+    citations = []
 
-    _, start, end = t_r.split(' ')
+    for line in (s for s in soff if s.split('\t', 2)[1].split()[0] in remove):
+        # Parse the line
+        _, t_r, __ = line.split('\t', 2)
 
-    start, end = int(start), int(end)
+        _, start, end = t_r.split(' ')
 
-    if start != end:
-        citations.append((start, end))
+        start, end = int(start), int(end)
 
-# Keep only the lines that correspond to a tag in 'keep'
-entries = (s for s in soff if s.split('\t', 2)[1].split()[0] in keep)
+        if start != end:
+            citations.append((start, end))
 
-secs = [] # Stack of sections
-title_seen = False
-sm = fig = 1
+    # Keep only the lines that correspond to a tag in 'keep'
+    entries = (s for s in soff if s.split('\t', 2)[1].split()[0] in keep)
 
-for ix, e in enumerate(entries):
+    secs = [] # Stack of sections
+    title_seen = False
+    sm = fig = 1
 
-    # Parse the line
-    _, t_r, __, attrs = e.split('\t')
-    tag, start, end = t_r.split(' ')
-    start = int(start)
-    end = int(end)
+    for ix, e in enumerate(entries):
 
-    if tag in ('article-title', 'abstract'):
+        # Parse the line
+        _, t_r, __, attrs = e.split('\t')
+        tag, start, end = t_r.split(' ')
+        start = int(start)
+        end = int(end)
 
-        # This is to enforce only one "article-title" (the actual title of the paper tag per file.
-        # There are other instance of the tag in the references
+        if tag in ('article-title', 'abstract'):
 
-        if tag == 'article-title':
-            if title_seen:
-                continue
+            # This is to enforce only one "article-title" (the actual title of the paper tag per file.
+            # There are other instance of the tag in the references
+
+            if tag == 'article-title':
+                if title_seen:
+                    continue
+                else:
+                    title_seen = True
+
+            print '%i\t%s\t%s\t%i\t%s' % (ix, nil, tag, 1 if tag == 'article-title' else 0, get_text(txt, start, end, citations))
+
+        elif tag in ('sec', 'fig', 'supplementary-material'):
+            # For our purposes, sections and figures are equivalent
+
+            # Parse the attributes
+            attrs = {k:v.strip('"') for k, v in ((s.split('=') for s in attrs.split()))}
+
+            # Figure out the section id
+            if tag == 'sec':
+                sec = attrs['id'] if 'id' in attrs else nil
+                sec_norm = attrs['sec-type'] if 'sec-type' in attrs else nil
+            elif tag == 'fig':
+                sec = 'fig-%i' % fig
+                sec_norm = sec
+                fig += 1
+            elif tag == 'supplementary-material':
+                sec = 'supm-%i' % sm
+                sec_norm = sec
+                sm += 1
             else:
-                title_seen = True
+                raise Exception("I shouldn't be here")
 
-        print '%i\t%s\t%s\t%i\t%s' % (ix, nil, tag, 1 if tag == 'article-title' else 0, get_text(txt, start, end, citations))
+            # Add this section to the sections stack
+            secs.append((sec, sec_norm))
 
-    elif tag in ('sec', 'fig', 'supplementary-material'):
-        # For our purposes, sections and figures are equivalent
+            # Remember the current span
+            current_span = (start, end)
 
-        # Parse the attributes
-        attrs = {k:v.strip('"') for k, v in ((s.split('=') for s in attrs.split()))}
+        elif tag in ('title', 'p'):
 
-        # Figure out the section id
-        if tag == 'sec':
-            sec = attrs['id'] if 'id' in attrs else nil
-            sec_norm = attrs['sec-type'] if 'sec-type' in attrs else nil
-        elif tag == 'fig':
-            sec = 'fig-%i' % fig
-            sec_norm = sec
-            fig += 1
-        elif tag == 'supplementary-material':
-            sec = 'supm-%i' % sm
-            sec_norm = sec
-            sm += 1
-        else:
-            raise Exception("I shouldn't be here")
+            # Only if we are in a section or image, otherwise this could be part of the references or metadata
+            if len(secs) > 0:
 
-        # Add this section to the sections stack
-        secs.append((sec, sec_norm))
-
-        # Remember the current span
-        current_span = (start, end)
-
-    elif tag in ('title', 'p'):
-
-        # Only if we are in a section or image, otherwise this could be part of the references or metadata
-        if len(secs) > 0:
-
-            if span_contains(current_span, (start, end)):
-                # If the current section's span contains this element
-                sec, sec_norm = secs[-1]
-            else:
-                # Pop the current section to return to the previous onse
-                secs.pop()
-                if len(secs) > 0:
+                if span_contains(current_span, (start, end)):
+                    # If the current section's span contains this element
                     sec, sec_norm = secs[-1]
                 else:
-                    # If there are no more sections in the stack, this element may not be useful information
-                    continue
+                    # Pop the current section to return to the previous onse
+                    secs.pop()
+                    if len(secs) > 0:
+                        sec, sec_norm = secs[-1]
+                    else:
+                        # If there are no more sections in the stack, this element may not be useful information
+                        continue
 
-            print '%i\t%s\t%s\t%i\t%s' % (ix, sec, sec_norm, 1 if tag == 'title' else 0, get_text(txt, start, end, citations))
+                print '%i\t%s\t%s\t%i\t%s' % (ix, sec, sec_norm, 1 if tag == 'title' else 0, get_text(txt, start, end, citations))
